77c66b40dd0a0722e2c941834b0464f03d13be91
→_→ <317286686@qq.com>
2021-08-10 08:25:09 +0800

    cppboard test

---
+++ tests/cppboard_test.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import random
+
+sys.path.append(os.path.realpath('../..'))
+from alpha_gomoku.cppboard import Board
+
+
+def main():
+    actions = [112, 97, 98, 113, 96, 95, 81, 66,
+			   80, 64, 111, 84, 126, 141, 127, 125]
+    actions = [(act // Board.BOARD_SIZE, act % Board.BOARD_SIZE)
+                for act in actions]
+    board = Board(actions)
+    print(board)
+    while not board.is_over:
+        action = random.choice(board.evaluate())
+        board.move(action)
+        print(board)
+        print(action)
+
+
+if __name__ == '__main__':
+    main()
