e3e8d8805f7e49373c450a9e5144887e5afa1f1f
→_→ <317286686@qq.com>
2021-09-03 22:35:30 +0800

    simplify predictor

--- alpha_gomoku/gcn/models/residual.py
+++ alpha_gomoku/gcn/models/residual.py
@@ -104,12 +104,7 @@ class GraphConvolutionNetwork(NetworkBase):
         self.backbone = nn.Sequential(*layers)
         in_dim *= GraphResidualBlock.expansion
         self.classifier = GraphConvolutionLayer(in_dim, 1, radius)
-        dim = hidden_dim * 4
-        self.predictor = initialize(nn.Sequential(
-            nn.Linear(in_dim, dim), nn.ReLU(), 
-            nn.Linear(dim, dim), nn.ReLU(), 
-            nn.Linear(dim, 1, bias=False)
-        ))
+        self.predictor = initialize(nn.Linear(in_dim, 1, bias=False))
     
     def forward(self, x):
         features = self.backbone(x)
