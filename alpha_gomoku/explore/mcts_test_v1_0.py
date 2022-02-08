54c063488c82dd9572cdbe82e2b1b1fabe9ad707
You Huang <youhuang0607@gmail.com>
2022-02-08 23:00:21 +0800

    analyze mcts

--- alpha_gomoku/explore/mcts_test_v1_0.py
+++ alpha_gomoku/explore/mcts_test_v1_0.py
@@ -31,18 +31,29 @@ def main():
     model = vi.get_model(model, weight_paths[-1])
     player = get_input('black(0) or white(1) ? ', int, ['0', '1'])
     board = Board()
-    print(board)
     if player == 0:
+        print(board)
         action = get_board_input(board)
         board.move(action)
-    mcts = vi.MCTS(model, [board], visit_times=200, cpuct=1.0,
-                   verbose=1, max_node_num=100000)
+    mcts = vi.MCTS(model, [board], visit_times=1000, cpuct=2.5,
+                   verbose=1, max_node_num=300000)
     while not board.is_over:
         print(board)
+        if board.player != player:
+            action = mcts.search()[0]
+        root = mcts.node_tables[0].get(board.key, None)
+        if root is None:
+            print('not found root')
+        else:
+            children = sorted(root.children.items(),
+                              key=lambda x: x[1].visit, reverse=True)
+            total_visit = sum(n.visit for act, n in children)
+            info = [f'{act}: p {n.prob:.2f} v {n.visit:.0f} s {n.score:.2f} ' +
+                    f'b {n.visit_bonus(total_visit):.2f}'
+                    for act, n in children[:5]]
+            print('\n'.join(info))
         if board.player == player:
             action = get_board_input(board)
-        else:
-            action = mcts.search()[0]
         board.move(action)
         mcts.move([action])
     print(board)
