e8d562130f53b557b67bbbb08f8c66e1c8bfb8a3
You Huang <youhuang0607@gmail.com>
2022-02-12 23:22:20 +0800

    cppboard setup

--- demo/cppboard/setup.py
+++ demo/cppboard/setup.py
@@ -5,8 +5,15 @@ from distutils.core import setup, Extension
 cpp_dir = Path(__file__).resolve().parents[0] / 'bitboard'
 cpp_files = ['board_wrap.cxx', 'board.cpp', 'board_bits.cpp',
              'init.cpp', 'lineshapes.cpp', 'pns.cpp', 'shapes.cpp']
-module = Extension('_board', sources=[str(cpp_dir / f) for f in cpp_files],
-                   extra_compile_args=['/O2'], language='c++')
+sources = [str(cpp_dir / f) for f in cpp_files]
+
+# optimization flag for msvc compiler in windows
+extra_compile_args = ['/O2']
+# optimization flag for gcc compiler in linux
+# extra_compile_args = ['-O3']
+
+module = Extension('_board', sources=sources,
+                   extra_compile_args=extra_compile_args, language='c++')
 
 
 setup(name='board', ext_modules=[module], py_modules=['board'])
