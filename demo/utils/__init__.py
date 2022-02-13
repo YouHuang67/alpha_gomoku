6e38ac4ee86ee8705ebd670ed4d3cc22c4acd44f
You Huang <youhuang0607@gmail.com>
2022-02-13 09:49:19 +0800

    reasign constants

--- demo/utils/__init__.py
+++ demo/utils/__init__.py
@@ -5,13 +5,5 @@ from .dihedral_reflection import *
 
 ROOT_DIR = Path(__file__).resolve().parents[0]
 WEIGHT_DIR = ROOT_DIR / 'weights'
-BOARD_SIZE = 15
-STONE_NUM = BOARD_SIZE ** 2
-BLACK = 0
-WHITE = 1
-EMPTY = 2
-OPEN_FOUR = 1
-FOUR = 2
-OPEN_THREE = 3
-THREE = 4
-OPEN_TWO = 5
+BOARD_SIZE, STONE_NUM = 15, 15 ** 2
+BLACK, WHITE, EMPTY = 0, 1, 2
